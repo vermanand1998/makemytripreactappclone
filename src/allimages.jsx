4c0787f30a0b9d23dab5945d103c03414becaab1
vermanand1998 <vermanand1998@gmail.com>
2024-02-10 23:40:55 +0530

    fffffffff

--- src/allimages.jsx
+++ src/allimages.jsx
@@ -1,1 +1,2 @@
-export const logoblue="../public/assets/Images/LogoBlue.jpg"
+export const logoblue="../public/assets/Images/LogoBlue.jpg";
+export const hotelroombed="../public/assets/Images/hotelroombed.jpg"
